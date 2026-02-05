b94412fce7cf11074019629ac5034ab71fb18245
Shishir2402 <shishirshriwastav24@gmail.com>
2026-02-05 22:51:48 +0530

    coderefactor

--- src/main/java/com/example/bookmyplan/flight.java
+++ src/main/java/com/example/bookmyplan/flight.java
@@ -1,4 +1,13 @@
 package com.example.bookmyplan;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class flight {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookmyplanApplication.class, args);
+    }
+
 }
