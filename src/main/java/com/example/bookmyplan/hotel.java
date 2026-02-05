0aaeb053b936be3b6d76a8bf2ed7a25a3cf3a5de
Shishir2402 <shishirshriwastav24@gmail.com>
2026-02-05 23:21:04 +0530

    adding new java class

--- src/main/java/com/example/bookmyplan/hotel.java
+++ src/main/java/com/example/bookmyplan/hotel.java
@@ -4,9 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class flight {
-    @GetMapping("/flight")
+public class hotel {
+    @GetMapping("/hotel")
     public String getData() {
-        return "Hello Welcome to bookmyplan, Please book flight ticket at 10% discount" ;
+        return "Hello Welcome to bookmyplan, Please book hotel at 10% discount" ;
     }
 }
