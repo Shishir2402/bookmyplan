d23b23b749b84b2605a43ee862974a37c5bedbf9
Shishir2402 <shishirshriwastav24@gmail.com>
2026-02-22 23:41:39 +0530

    hello

---
+++ src/main/java/com/example/bookmyplan/visa.java
@@ -0,0 +1,14 @@
+package com.example.bookmyplan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class visa {
+    @GetMapping("/visa")
+    public String getData() {
+        return "Hello, Welcome to bookmyplan, Please boot flight ticket at 10% discount";
+    }
+}
