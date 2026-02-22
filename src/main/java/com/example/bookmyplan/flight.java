b9f184164f71a0fd66d9e13bd5793ca07839027a
Shishir2402 <shishirshriwastav24@gmail.com>
2026-02-22 12:44:19 +0530

    adding new javaclass

---
+++ src/main/java/com/example/bookmyplan/flight.java
@@ -0,0 +1,14 @@
+package com.example.bookmyplan;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+    public class flight {
+    @GetMapping("/flight")
+    public String getData() {
+        return "Hello, Welcome to bookmyplan, Please boot flight ticket at 10% discount";
+    }
+}
